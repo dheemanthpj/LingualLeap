eac898d303587342c3fd341bae464ab4a6f5e8d3
Dheemanth Chettiappa <ponnachanadheemanth@gmail.com>
2025-09-01 01:06:51 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // IMPORTANT: Replace this with the actual config from your Firebase project
 const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID"
+  "projectId": "lingualeap-xapen",
+  "appId": "1:118368504355:web:3610222112e58748453f96",
+  "storageBucket": "lingualeap-xapen.firebasestorage.app",
+  "apiKey": "AIzaSyD8xPdomDYX4eKg4bwWKp7ifybjRf28rME",
+  "authDomain": "lingualeap-xapen.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "118368504355"
 };
 
 
